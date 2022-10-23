2237d10984de1e16a9f7ed904d3259fc0731dc86
Felix Eggert <felix.eggert@real-digital.de>
2022-10-23 13:43:10 +0200

    starting and stopping the world

--- gameserver/src/systems/world.js
+++ gameserver/src/systems/world.js
@@ -1,33 +1,74 @@
 const {Entity} = require('../entities/entity');
 const {MovementSystem} = require('./movementsystem');
 
+
+const SYSTEMS = [
+    new MovementSystem(),
+]
+
+
+const ProcessStatus = {
+    RUNNING: "Running",
+    STOPPED: "Stopped",
+}
+
+
 class World {
 
-    #systems = [
-        new MovementSystem()
-    ];
+    #systems = SYSTEMS;
     #entities = [];
+    #time = 0
+    #processHandle = null;
     tickrate = 1 / 10
-    running = true
 
     get entities() {
         return this.#entities;
     }
 
-    process() {
+    get time() {
+        return this.#time;
+    }
+
+    get processStatus() {
+        return this.#processHandle === null ? ProcessStatus.STOPPED : ProcessStatus.RUNNING;
+    }
+
+    startProcess() {
+        if (this.processStatus === ProcessStatus.RUNNING) {
+            throw new RunningProcessError(`Process loop is already running`)
+        }
+        this.#processHandle = setInterval(this.#processLoop.bind(this), this.tickrate * 1000)
+    }
+
+    stopProcess() {
+        if (this.processStatus === ProcessStatus.STOPPED) {
+            throw new RunningProcessError(`Process loop is already stopped`)
+        }
+        clearInterval(this.#processHandle)
+        this.#processHandle = null;
+    }
+
+    #processLoop() {
+        if (this.processStatus === ProcessStatus.STOPPED) {
+            throw new RunningProcessError("Something went terribly wrong");
+        }
+
+        this.#time++;
         this.#systems.forEach(system => {
             system.process(this.#entities, {})
         })
-        if (this.running) setTimeout(this.process.bind(this), this.tickrate)
     }
-
 
     static loadFromJson(json) {
         let world = new World();
-        world.#entities.push(...json["entities"].map(Entity.loadFromJson))
+        world.#time = json.time
+        world.#entities.push(...json.entities.map(Entity.loadFromJson))
         return world;
     }
 
 }
 
-module.exports = {World}
+class RunningProcessError extends Error {
+}
+
+module.exports = {World, ProcessStatus, RunningProcessError}
--- gameserver/src/systems/world.test.js
+++ gameserver/src/systems/world.test.js
@@ -3,10 +3,11 @@ const {Component} = require('../components/component');
 const {Entity} = require('../entities/entity');
 const {arrayEquals} = require('../utils/arrayutils')
 
-const {World} = require('./world')
+const {World, ProcessStatus, RunningProcessError} = require('./world')
 
 test('World Loading', () => {
     let w = World.loadFromJson({
+        time: 0,
         entities: [
             {
                 id: "bananas-id",
@@ -32,3 +33,35 @@ test('World Loading', () => {
 });
 
 
+test('Processing', async () => {
+    let w = World.loadFromJson({
+        time: 20,
+        entities: []
+    })
+
+    expect(w.time).toBe(20);
+    expect(w.processStatus).toBe(ProcessStatus.STOPPED)
+
+    w.startProcess()
+    expect(w.processStatus).toBe(ProcessStatus.RUNNING)
+    expect(() => w.startProcess()).toThrow(RunningProcessError);
+    expect(w.processStatus).toBe(ProcessStatus.RUNNING)
+    expect(w.time).toBe(20);
+
+    await sleep(1);
+    let time = w.time;
+    expect(time).toBe(30);
+    expect(w.processStatus).toBe(ProcessStatus.RUNNING)
+
+    w.stopProcess();
+    expect(w.processStatus).toBe(ProcessStatus.STOPPED);
+
+    await sleep(0.2);
+    expect(w.processStatus).toBe(ProcessStatus.STOPPED);
+    expect(w.time).toBe(time);
+});
+
+
+function sleep(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
