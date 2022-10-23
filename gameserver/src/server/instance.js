3d05f2bb9b0a41bd81e8dd757f605243c48442a4
Felix Eggert <felix.eggert@real-digital.de>
2022-10-23 16:48:05 +0200

    added instance

---
+++ gameserver/src/server/instance.js
@@ -0,0 +1,37 @@
+const {World} = require('../systems/world');
+
+class Instance {
+
+    world = null;
+
+    constructor(parameters) {
+        this.#world = World.loadFromJson({
+            time: 0,
+            entities: [
+                {
+                    id: "bananas-id",
+                    components: [
+                        {
+                            id: "abc",
+                            type: "Position",
+                            data: {x: 1, y: 1}
+                        },
+                        {
+                            id: "abcd",
+                            type: "Movement",
+                            data: {x: 10, y: 10, speed: 1}
+                        }
+                    ]
+                }
+            ]
+        })
+
+    }
+
+    getState() {
+
+    }
+
+}
+
+module.exports = {Instance}
