397ed288c753a5996518547bf98645a676162052
Felix Eggert <felix.eggert@real-digital.de>
2022-10-23 16:20:14 +0200

    more 3js

--- app/src/app/gamescreen.js
+++ app/src/app/gamescreen.js
@@ -3,10 +3,18 @@ import Receiver from "./receiver"
 
 class GameScreen {
 
+    camera = null;
+
+    width = 800;
+    height = 600;
+
     constructor(container) {
 
-        let camera = new Three.PerspectiveCamera(70, 4 / 3, 0.01, 10);
-        camera.position.z = 1;
+        let r = new Receiver();
+
+        this.camera = new Three.PerspectiveCamera(70, this.width / this.height, 0.01, 10);
+        this.camera.position.z = 4;
+        this.camera.lookAt(0, 0, 0)
 
         let scene = new Three.Scene();
 
@@ -16,36 +24,92 @@ class GameScreen {
         let mesh = new Three.Mesh(geometry, material);
         scene.add(mesh);
 
+
+        const axesHelper = new Three.AxesHelper(5);
+        scene.add(axesHelper);
+
         let renderer = new Three.WebGLRenderer({antialias: true});
-        renderer.setSize(600, 400);
-        //let container = document.getElementById('main-canvas');
+        renderer.setSize(this.width, this.height);
         container.appendChild(renderer.domElement);
+
+
+        const planeGeom = new Three.PlaneGeometry(5, 5);
+        const planeMat = new Three.MeshBasicMaterial({color: 0x2255aa, side: Three.DoubleSide});
+        const plane = new Three.Mesh(planeGeom, planeMat);
+        scene.add(plane);
+
+
+        let cam = this.camera;
 
         function animate() {
             requestAnimationFrame(animate);
             //console.log("render element " + container)
             mesh.rotation.x += 0.01;
-            //mesh.rotation.y += 0.02;
-            renderer.render(scene, camera);
+            mesh.rotation.y += 0.02;
+            renderer.render(scene, cam);
 
         }
 
         animate()
 
-        this.connection = new WebSocket("ws://localhost:8080")
+        document.addEventListener('keydown', this.moveCamera.bind(this));
 
-        this.connection.onmessage = function (event) {
-            console.log(event);
+
+        let onDocumentMouseDown = (event) => {
+            event.preventDefault();
+
+            //console.log(event)
+            let raycaster = new Three.Raycaster();
+            let pointer = new Three.Vector2();
+            pointer.x = (event.layerX / this.width) * 2 - 1;
+            pointer.y = -(event.layerY / this.height) * 2 + 1;
+
+            this.camera.updateMatrixWorld();
+            raycaster.setFromCamera(pointer, this.camera);
+
+
+            const intersects = raycaster.intersectObjects([mesh], false);
+
+
+            if (intersects.length > 0) {
+                console.log(`[${event.layerX}/${event.layerY}] yayyy`, intersects)
+            } else {
+                console.log(`[${event.layerX}/${event.layerY}] :(((`)
+            }
+
+
         }
 
-        this.connection.onopen = function (event) {
-            console.log(event)
-            console.log("Successfully connected to the echo websocket server...")
-        }
+
+        container.addEventListener('mousedown', onDocumentMouseDown);
+
 
     }
 
+    moveCamera(evt) {
 
+        switch (evt.key) {
+            case "a":
+                this.camera.position.x -= 0.1;
+                break;
+            case "d":
+                this.camera.position.x += 0.1;
+                break;
+            case "w":
+                this.camera.position.y += 0.1;
+                break;
+            case "s":
+                this.camera.position.y -= 0.1;
+                break;
+            case "q":
+                this.camera.rotateX(0.1);
+                break;
+            case "e":
+                this.camera.rotateX(-0.1);
+                break;
+        }
+
+    }
 }
 
 export default GameScreen
--- app/src/app/receiver.js
+++ app/src/app/receiver.js
@@ -3,17 +3,17 @@ class Receiver {
     constructor() {
 
 
-        console.log("Starting connection to WebSocket Server")
-        this.connection = new WebSocket("wss://echo.websocket.org")
+        this.connection = new WebSocket("ws://localhost:8080")
 
-        this.connection.onmessage = function (event) {
-            console.log(event);
-        }
+        //this.connection.onmessage = function (event) {
+        //    console.log(event);
+        //}
 
         this.connection.onopen = function (event) {
             console.log(event)
             console.log("Successfully connected to the echo websocket server...")
         }
+
     }
 }
 
